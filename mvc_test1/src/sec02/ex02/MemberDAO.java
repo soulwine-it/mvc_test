590bebd28a1a454564e01bbe35b79751d5737d3e
soulwine-it <skyorwind@gmail.com>
2019-12-30 20:49:03 +0900

    update 회원정보 수정 삭제

--- mvc_test1/src/sec02/ex02/MemberDAO.java
+++ mvc_test1/src/sec02/ex02/MemberDAO.java
@@ -1,5 +1,4 @@
 package sec02.ex02;
-
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -16,7 +15,7 @@ public class MemberDAO {
 	private DataSource dataFactory;
 	private Connection conn;
 	private PreparedStatement pstmt;
-	
+
 	public MemberDAO() {
 		try {
 			Context ctx = new InitialContext();
@@ -26,6 +25,7 @@ public class MemberDAO {
 			e.printStackTrace();
 		}
 	}
+
 	public List<MemberVO> listMembers() {
 		List<MemberVO> membersList = new ArrayList();
 		try {
@@ -51,6 +51,7 @@ public class MemberDAO {
 		}
 		return membersList;
 	}
+
 	public void addMember(MemberVO m) {
 		try {
 			conn = dataFactory.getConnection();
@@ -58,20 +59,78 @@ public class MemberDAO {
 			String pwd = m.getPwd();
 			String name = m.getName();
 			String email = m.getEmail();
-			String query = "INSERT INTO t_member(id, pwd, name, email)"+"values(?,?,?,?)";
+			String query = "INSERT INTO t_member(id, pwd, name, email)" + " VALUES(?, ? ,? ,?)";
 			System.out.println(query);
 			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1,  id);
+			pstmt.setString(1, id);
 			pstmt.setString(2, pwd);
 			pstmt.setString(3, name);
 			pstmt.setString(4, email);
 			pstmt.executeUpdate();
 			pstmt.close();
 			conn.close();
-			} catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public MemberVO findMember(String _id) {
+		MemberVO memInfo = null;
+		try {
+			conn = dataFactory.getConnection();
+			String query = "select * from  t_member where id=?";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, _id);
+			System.out.println(query);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			String id = rs.getString("id");
+			String pwd = rs.getString("pwd");
+			String name = rs.getString("name");
+			String email = rs.getString("email");
+			Date joinDate = rs.getDate("joinDate");
+			memInfo = new MemberVO(id, pwd, name, email, joinDate);
+			pstmt.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return memInfo;
+	}
+
+	public void modMember(MemberVO memberVO) {
+		String id = memberVO.getId();
+		String pwd = memberVO.getPwd();
+		String name = memberVO.getName();
+		String email = memberVO.getEmail();
+		try {
+			conn = dataFactory.getConnection();
+			String query = "update t_member set pwd=?,name=?,email=?  where id=?";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, pwd);
+			pstmt.setString(2, name);
+			pstmt.setString(3, email);
+			pstmt.setString(4, id);
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void delMember(String id) {
+		try {
+			conn = dataFactory.getConnection();
+			String query = "delete from t_member where id=?";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1,id);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
