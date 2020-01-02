35ae3363b88d67cdd2c79435384e822d20afd36c
soulwine-it <skyorwind@gmail.com>
2020-01-02 17:06:39 +0900

    UPDATE 게시판 구현

--- mvc_test1/src/sec03/brd01/BoardController.java
+++ mvc_test1/src/sec03/brd01/BoardController.java
@@ -33,22 +33,29 @@ public class BoardController extends HttpServlet {
 	}
 
 	private void doHandle(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+		//포워딩할 객체 nextPage 생성
 		String nextPage = "";
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
+		
+		// 클라리언트가 request를 보낼 때의 URL 관련된 부가적인 경로 정보를 리턴한다. 부가적인 경로 정보는 서블릿 경로
+		//요청명을 가져옴
 		String action = request.getPathInfo();
 		System.out.println("action:" + action);
+		
 		try {
 			List<ArticleVO> articlesList = new ArrayList<ArticleVO>();
 			if (action == null) {
 				articlesList = boardService.listArticles();
 				request.setAttribute("articlesList", articlesList);
 				nextPage = "/board01/listArticles.jsp";
+				
 			} else if (action.equals("/listArticles.do")) {
 				articlesList = boardService.listArticles();
 				request.setAttribute("articlesList", articlesList);
 				nextPage = "/board01/listArticles.jsp";
 			}
+			
 			RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
 			dispatch.forward(request, response);
 		} catch (Exception e) {
